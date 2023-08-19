e0c5adc86aa7939d93b017038cb92af04aadcaf4
MH_Anik <65283345+mahedi-anik@users.noreply.github.com>
2023-08-19 21:35:11 +0600

    Admin Dashboard

--- main-content.php
+++ main-content.php
@@ -1,75 +1,90 @@
 
 <div class="main-content">
 		      <div class="row">
-			     <div class="col-lg-3 col-md-6 col-sm-6">
+			     <div class="col-lg-4 col-md-6 col-sm-6">
 				    <div class="card card-stats">
 					   <div class="card-header">
 					      <div class="icon icon-warning">
-						     <span class="material-icons">equalizer</span>
+						     <span class="material-icons">people</span>
 						  </div>
 					   </div>
 					   <div class="card-content">
 					       <p class="category"><strong>Total Teachers</strong></p>
-						   <h3 class="card-title">
+						   <h1 class="card-title">
 						   	<?php
-@include 'config.php';
+											@include 'config.php';
 
-$sql = "SELECT COUNT(*) as count FROM users where role='teacher' ";
-$result = $conn->query($sql);
-$my=$result['count'];
-echo $my;
-?>
-						    </h3>
+											$query=mysqli_query($conn,"SELECT count(id) as id FROM `users` WHERE role='teacher'  ");
+											while($row=mysqli_fetch_array($query)){
+											?>
+											<?php echo ucwords($row['id']); ?>
+											<?php
+											}
+
+										?>
+						    </h1>
 					   </div>
 	
 					</div>
 				 </div>
 				 
-				 <div class="col-lg-3 col-md-6 col-sm-6">
+				 <div class="col-lg-4 col-md-6 col-sm-6">
 				    <div class="card card-stats">
 					   <div class="card-header">
 					      <div class="icon icon-rose">
-						     <span class="material-icons">shopping_cart</span>
+						     <span class="material-icons">people</span>
 						  </div>
 					   </div>
 					   <div class="card-content">
 					       <p class="category"><strong>Total Students</strong></p>
-						   <h3 class="card-title">102</h3>
+						   <h1 class="card-title">
+						   	<?php
+											@include 'config.php';
+
+											$query=mysqli_query($conn,"SELECT count(id) as id FROM `users` WHERE role='student' ");
+											while($row=mysqli_fetch_array($query)){
+											?>
+											<?php echo ucwords($row['id']); ?>
+											<?php
+											}
+
+										?>
+
+						   </h1>
 					   </div>
 					   
 					</div>
 				 </div>
 				 
-				 <div class="col-lg-3 col-md-6 col-sm-6">
+				 <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="card card-stats">
                                 <div class="card-header">
                                     <div class="icon icon-success">
-                                        <span class="material-icons">attach_money</span>
+                                        <span class="material-icons">equalizer</span>
 
                                     </div>
                                 </div>
                                 <div class="card-content">
                                     <p class="category"><strong>Total Departments</strong></p>
-                                    <h3 class="card-title">$23,100</h3>
+                                    <h1 class="card-title">
+                                    	<?php
+											@include 'config.php';
+
+											$query=mysqli_query($conn,"SELECT count(departmentid) as id FROM `department` ");
+											while($row=mysqli_fetch_array($query)){
+											?>
+											<?php echo ucwords($row['id']); ?>
+											<?php
+											}
+
+										?>
+
+                                    </h1>
                                 </div>
                                 
                             </div>
                         </div>
-				  <div class="col-lg-3 col-md-6 col-sm-6">
-				    <div class="card card-stats">
-					   <div class="card-header">
-					      <div class="icon icon-info">
-						     <span class="material-icons">follow_the_signs</span>
-						  </div>
-					   </div>
-					   <div class="card-content">
-					       <p class="category"><strong>Total Sections</strong></p>
-						   <h3 class="card-title">+245</h3>
-					   </div>
-					</div>
-				 </div>
-				 
-				 
+				  
 			  </div>
 			  
 			  
@@ -78,16 +93,17 @@ echo $my;
 			  
 			  <div class="row">
 			     <div class="col-lg-6 col-md-12">
-				   <div class="card" style="min-height:300px">
+				   <div class="card" >
 				       <div class="card-header card-header-text">
-					      <h4 class="card-title">Admins Stats</h4>
+					      <h1 class="card-title">Department Admin's List</h1>
+					      <hr>
 					   </div>
 					   <div class="card-content table-responsive">
 
 					       <table class="table table-hover">
 						      <thead class="text-primary">
 								 <th>Name</th>
-								 <th>Email</th>
+								 <th>Department</th>
 								 <th>Mobile</th>
 								 </tr>
 							  </thead>
@@ -95,12 +111,12 @@ echo $my;
 							     <?php
 											@include 'config.php';
 
-											$query=mysqli_query($conn,"SELECT * FROM `users` WHERE role='department_admin' order by name asc ");
+											$query=mysqli_query($conn,"SELECT * from users left join department on users.departmentid=department.departmentid left join course on users.courseid=course.courseid where users.role='department_admin' order by id desc,name asc limit 5 ");
 											while($row=mysqli_fetch_array($query)){
 											?>
 											<tr>
 												<td><?php echo ucwords($row['name']); ?></td>
-												<td><?php echo ucwords($row['email']); ?></td>
+												<td><?php echo ucwords($row['departmentname']); ?></td>
 												<td><?php echo ucwords($row['mobile']); ?></td>
 												
 											</tr>
@@ -118,9 +134,10 @@ echo $my;
 		   </div>
 		   
 		   <div class="col-lg-6 col-md-12">
-		      <div class="card" style="min-height:300px">
+		      <div class="card" >
 			     <div class="card-header card-header-text">
-                   <h4 class="card-title">Teacher Status</h4>
+                   <h1 class="card-title">Teacher's List</h1>
+                   <hr>
                  </div>				 
 			 
 			  <div class="card-content table-responsive">
@@ -128,21 +145,21 @@ echo $my;
 					       <table class="table table-hover">
 						      <thead class="text-primary">
 								 <th>Name</th>
-								 <th>Email</th>
-								 <th>Mobile</th>
+								 <th>Department</th>
+								 <th>Course</th>
 								 </tr>
 							  </thead>
 							  <tbody>
 							     <?php
 											@include 'config.php';
 
-											$query=mysqli_query($conn,"SELECT * FROM `users` WHERE role='teacher' order by name asc ");
+											$query=mysqli_query($conn,"SELECT * from users left join department on users.departmentid=department.departmentid left join course on users.courseid=course.courseid where users.role='teacher' order by id desc,name asc limit 5 ");
 											while($row=mysqli_fetch_array($query)){
 											?>
 											<tr>
 												<td><?php echo ucwords($row['name']); ?></td>
-												<td><?php echo ucwords($row['email']); ?></td>
-												<td><?php echo ucwords($row['mobile']); ?></td>
+												<td><?php echo ucwords($row['departmentname']); ?></td>
+												<td><?php echo ucwords($row['course']); ?></td>
 												
 											</tr>
 											<?php
@@ -154,34 +171,45 @@ echo $my;
 					   </div>
 		   </div> 
 		</div>
+	</div>
 		<div class="row">
 			<div class="col-lg-12 col-md-12">
-		      <div class="card" style="min-height:300px">
+		      <div class="card">
 			     <div class="card-header card-header-text">
-                   <h4 class="card-title">Student Status</h4>
+                   <h1 class="card-title">Student's List</h1>
+                   <hr>
                  </div>				 
 			 
 			  <div class="card-content table-responsive">
 
 					       <table class="table table-hover">
 						      <thead class="text-primary">
-								 <th>Name</th>
-								 <th>Email</th>
-								 <th>Mobile</th>
+								 <tr>
+											<th>Name</th>
+											<th>Department</th>
+											<th>Session</th>
+											<th>Section</th>
+											<th>Email</th>
+											<th>Mobile</th>
+											<th>Status</th>
+										</tr>
 								 </tr>
 							  </thead>
 							  <tbody>
 							     <?php
 											@include 'config.php';
 
-											$query=mysqli_query($conn,"SELECT * FROM `users` WHERE role='student' order by name asc ");
+											$query=mysqli_query($conn,"SELECT * from users left join department on users.departmentid=department.departmentid left join session on users.sessionid=session.sessionid left join section on users.sectionid=section.sectionid where users.role='student' order by id desc,name asc limit 10 ");
 											while($row=mysqli_fetch_array($query)){
 											?>
 											<tr>
-												<td><?php echo ucwords($row['name']); ?></td>
+												<td><?php echo ucwords($row['name']); ?></td>										
+												<td><?php echo ucwords($row['departmentname']); ?></td>
+												<td><?php echo ucwords($row['session']); ?></td>
+												<td><?php echo ucwords($row['sectionname']); ?></td>
 												<td><?php echo ucwords($row['email']); ?></td>
 												<td><?php echo ucwords($row['mobile']); ?></td>
-												
+												<td><?php echo ucwords($row['status']); ?></td>
 											</tr>
 											<?php
 											}
